8f437d78c16d632d343576dc37e34cc0af3fb33e
Pavel Kobjakov <Ashymonth@gmail.com>
2020-07-31 20:12:01 +0300

    Added wpf project

---
+++ CrudTestAssignment.Ui/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using CrudTestAssignment.Ui.Views;
+using System.Windows;
+
+namespace CrudTestAssignment.Ui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
